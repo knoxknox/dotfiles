34d8b10c01abb43e967961246e1f31f3446498fe
knoxknox <alexandr_y@mail.ru>
2018-10-06 13:38:54 +0300

    Add apt.sh

---
+++ bin/apt.sh
@@ -0,0 +1,59 @@
+# bin
+apt-get install mc
+apt-get install git
+apt-get install vim
+apt-get install curl
+apt-get install htop
+apt-get install wget
+apt-get install byobu
+apt-get install unzip
+apt-get install docker
+apt-get install vagrant
+
+# pip
+sudo pip install httpie
+sudo pip install colout
+
+# node
+install nvm
+add nvm.sh to ~/.bashrc
+node: nvm install 0.10.31
+nvm alias default 0.10.31
+gulp: npm install -g gulp
+bower: npm install -g bower
+grunt: npm install -g grunt-cli
+packages: npm install && bower install
+
+# php5
+apt-get install php5
+apt-get install apache2
+apt-get install php5-mysql
+apt-get install phpmyadmin
+debugger: http://phpdbg.com
+php5-composer: https://getcomposer.org
+
+# ruby
+apt-get install ruby1.9.3
+curl -sSL https://get.rvm.io | bash -s stable
+.rvmrc: rvm use 2.0.0@yourgemsetname --create
+
+# storage
+apt-get install memcached
+apt-get install mysql-server
+apt-get install redis-server
+
+# packages
+sudo dpkg -i skype-linux.deb
+sudo dpkg -i teamviewer-linux.deb
+sudo dpkg -i dropbox_1.6.2_amd64.deb
+sudo dpkg -i google-chrome-stable.deb
+sudo dpkg -i mysql-workbench-community.deb
+tar -zxvf meld-3.16.3.tar.xz
+tar -zxvf sublime-text-3_0_0.tar.gz
+tar -zxvf smartgit-linux-8_0_2.tar.gz
+(curl -L -s get.jenv.io | bash) && jenv install java 1.8.0_71
+wget https://bootstrap.pypa.io/get-pip.py && python get-pip.py
+
+# gnome-classic
+apt-get install gnome-shell or gnome-session-fallback
+gsettings set org.gnome.desktop.wm.preferences button-layout ':minimize,maximize,close'
