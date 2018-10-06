6a0946dc6442475bce21275f72bb58997101e706
knoxknox <alexandr_y@mail.ru>
2018-10-06 13:13:44 +0300

    Add brew.sh

---
+++ bin/brew.sh
@@ -0,0 +1,18 @@
+brew install git
+brew install vim
+brew install wget
+brew install httpie
+
+brew cask install zoom
+brew cask install medis
+brew cask install docker
+brew cask install iterm2
+brew cask install vagrant
+brew cask install smartgit
+brew cask install apptivate
+brew cask install spectacle
+brew cask install viscosity
+brew cask install sequel-pro
+brew cask install virtualbox
+brew cask install sublime-text
+brew cask install google-chrome
