4add08c739c379161973c706dcea6b98e8a90f4f
knoxknox <alexandr_y@mail.ru>
2018-10-06 17:17:06 +0300

    Reorganize dev tools

--- bin/shared.sh
+++ bin/shared.sh
@@ -1,26 +1,29 @@
-# node
-install nvm
-add nvm.sh to ~/.bashrc
-node: nvm install 0.10.31
-nvm alias default 0.10.31
-gulp: npm install -g gulp
-bower: npm install -g bower
-grunt: npm install -g grunt-cli
-packages: npm install && bower install
+# jvm
+# jenv install java 1.6.0_45
+# jenv install java 1.7.0_75
+# jenv install java 1.8.0_77
+curl -L -s get.jenv.io | bash
 
-# php5
+# pvm
+cd /tmp/
+wget https://bootstrap.pypa.io/get-pip.py
+sudo python get-pip.py && rm /tmp/get-pip.py
+
+# php
 apt-get install php5
 apt-get install apache2
 apt-get install php5-mysql
 apt-get install phpmyadmin
-debugger: http://phpdbg.com
-php5-composer: https://getcomposer.org
+cd /tmp/
+curl -sS https://getcomposer.org/installer -o composer-setup.php
+sudo php composer-setup.php --install-dir=/usr/local/bin --filename=composer
 
-# ruby
-apt-get install ruby1.9.3
-curl -sSL https://get.rvm.io | bash -s stable
-.rvmrc: rvm use 2.0.0@yourgemsetname --create
+# nvm
+# nvm install 0.10.31
+# nvm alias default 0.10.31
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
 
-# packages
-(curl -L -s get.jenv.io | bash) && jenv install java 1.8.0_71
-wget https://bootstrap.pypa.io/get-pip.py && python get-pip.py
+# rvm
+url="hkp://keys.gnupg.net"
+key="409B6B1796C275462A1703113804BB82D39DC0E3"
+gpg --keyserver $url --recv-keys $key && curl -sSL https://get.rvm.io | bash -s stable
