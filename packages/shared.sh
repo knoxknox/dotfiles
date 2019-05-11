5eacdc5952163b5bc44d5fceeb7619b4efb17b7a
knoxknox <alexandr_y@mail.ru>
2019-05-11 15:54:50 +0300

    Update node LTS version

--- packages/shared.sh
+++ packages/shared.sh
@@ -25,8 +25,8 @@ curl -sS https://getcomposer.org/installer -o composer-setup.php
 sudo php composer-setup.php --install-dir=/usr/local/bin --filename=composer
 
 # nvm
-# nvm install 8.15.0
-# nvm alias default 8.15.0
+# nvm install 10.15.3
+# nvm alias default 10.15.3
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 
 # rvm
