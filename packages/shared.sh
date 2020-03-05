f600ee1be9303fef24c1d492a78487c7cade861f
knoxknox <alexandr_y@mail.ru>
2020-03-05 17:19:21 +0200

    Update jruby

--- packages/shared.sh
+++ packages/shared.sh
@@ -31,7 +31,7 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.2/install.sh | bash
 
 # rvm
 # rvm install ruby-2.6.5
-# rvm install jruby-9.2.9.0
+# rvm install jruby-9.2.11.0
 name="hkp://pool.sks-keyservers.net"
 keys="409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB"
 gpg --keyserver $name --recv-keys $keys && curl -sSL https://get.rvm.io | bash -s stable
