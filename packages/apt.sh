6a7a84d3a65b2148aca8570e33f6ef7e4eddce2b
knoxknox <alexandr_y@mail.ru>
2020-03-05 17:25:30 +0200

    Update apt packages

--- packages/apt.sh
+++ packages/apt.sh
@@ -13,14 +13,14 @@ apt-get -y install openvpn
 apt-get -y install vagrant
 
 # packages
-tar -xvf meld-3.20.1.tar.xz
-tar -zxvf smartgit-linux-19_1_5.tar.gz
+tar -xvf meld-3.20.2.tar.xz
+tar -zxvf smartgit-linux-19_1_7.tar.gz
 tar -jxvf sublime_text_3_build_3211_x64.tar.bz2
 sudo dpkg -i skypeforlinux-64.deb
 sudo dpkg -i virtualbox-6.0.6_amd64.deb
 sudo dpkg -i dropbox_2019.02.14_amd64.deb
 sudo dpkg -i google-chrome-stable-76.0_amd64.deb
-sudo dpkg -i mysql-workbench-community_8.0.17_amd64.deb
+sudo dpkg -i mysql-workbench-community_8.0.19_amd64.deb
 
 # gnome-classic
 apt-get install gnome-shell or gnome-session-fallback
