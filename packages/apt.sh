3707c900d46c6b73eea42f66d40ca1010d07c9e9
knoxknox <alexandr_y@mail.ru>
2020-09-18 20:08:28 +0300

    Update linux packages

--- packages/apt.sh
+++ packages/apt.sh
@@ -3,20 +3,22 @@ apt-get -y install mc
 apt-get -y install git
 apt-get -y install vim
 apt-get -y install curl
-apt-get -y install htop
 apt-get -y install tmux
 apt-get -y install wget
 apt-get -y install unzip
 apt-get -y install docker
 apt-get -y install httpie
 apt-get -y install openvpn
+apt-get -y install ripgrep
 apt-get -y install vagrant
+apt-get -y install fd-find
 
 # packages
 tar -xvf meld-3.20.2.tar.xz
 tar -zxvf smartgit-linux-20_1_4.tar.gz
 tar -jxvf sublime_text_3_build_3211_x64.tar.bz2
 sudo dpkg -i skypeforlinux-64.deb
+sudo dpkg -i watchexec-1.14-x86_64.deb
 sudo dpkg -i virtualbox-6.1.14_amd64.deb
 sudo dpkg -i dropbox_2020.03.04_amd64.deb
 sudo dpkg -i google-chrome-stable-85.0_amd64.deb
