aadd097adcfb7b3da755055ad9922a19817903da
knoxknox <alexandr_y@mail.ru>
2020-09-18 19:05:46 +0300

    Update packages versions

--- packages/shared.sh
+++ packages/shared.sh
@@ -11,8 +11,8 @@ sudo python3 get-pip.py && rm /tmp/get-pip.py
 
 # gvm
 cd /tmp/
-wget https://dl.google.com/go/go1.14.1.linux-amd64.tar.gz
-sudo tar -C /usr/local -xzf /tmp/go1.14.1.linux-amd64.tar.gz
+wget https://dl.google.com/go/go1.15.2.linux-amd64.tar.gz
+sudo tar -C /usr/local -xzf /tmp/go1.15.2.linux-amd64.tar.gz
 echo 'export PATH="$PATH:/usr/local/go/bin"' >> $HOME/.profile
 
 # php
@@ -25,13 +25,13 @@ curl -sS https://getcomposer.org/installer -o composer-setup.php
 sudo php composer-setup.php --install-dir=/usr/local/bin --filename=composer
 
 # nvm
-# nvm install 12.16.1
-# nvm alias default 12.16.1
+# nvm install 12.18.4
+# nvm alias default 12.18.4
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
 
 # rvm
-# rvm install ruby-2.6.6
-# rvm install jruby-9.2.11.1
+# rvm install ruby-2.7.1
+# rvm install jruby-9.2.13.0
 name="hkp://pool.sks-keyservers.net"
 keys="409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB"
 gpg --keyserver $name --recv-keys $keys && curl -sSL https://get.rvm.io | bash -s stable
