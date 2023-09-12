3783a897f386b489029626b003b0625e46ec6e3f
Alex <alexandr_y@hotmail.com>
2023-09-12 13:25:43 +0300

    Change git client (brew)

--- packages/brew.sh
+++ packages/brew.sh
@@ -17,8 +17,8 @@ brew cask install docker
 brew cask install iterm2
 brew cask install postico
 brew cask install vagrant
-brew cask install smartgit
 brew cask install sequel-pro
 brew cask install virtualbox
 brew cask install sublime-text
+brew cask install sublime-merge
 brew cask install google-chrome
